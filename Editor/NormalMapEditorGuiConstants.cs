7d778ad426e932c775330bacf128a3adc538760a
Razvan Luta <razvan.luta@gmail.com>
2021-07-12 23:16:52 -0700

    Set up a basic window for the tool.

---
+++ Editor/NormalMapEditorGuiConstants.cs
@@ -0,0 +1,11 @@
+namespace UnityNormalMapEditor.Editor
+{
+    public static class NormalMapEditorGuiConstants
+    {
+        public const string NormalMapEditorName = "Normal Map Editor";
+        public const string NormalMapEditorMenuItemPath = "Raz's Tools/Normal Map Editor";
+        public const string NormalMapEditorToolsUrl = "https://github.com/razluta/UnityNormalMapEditor";
+        
+        public const string RootUxmlPath = "CS_NormalMapEditor";
+    }
+}
---
+++ Editor/NormalMapEditorWindow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityNormalMapEditor.Editor.NormalMapEditorGuiConstants;
+
+namespace UnityNormalMapEditor.Editor 
+{
+    public class NormalMapEditorWindow : EditorWindow
+    {
+        private static readonly Vector2 NormalMapEditorWindowSize = new Vector2(350, 800);
+
+        private static VisualElement _root;
+
+
+        [MenuItem(NormalMapEditorMenuItemPath)]
+        public static void ShowWindow()
+        {
+            var window = GetWindow<NormalMapEditorWindow>();
+            window.titleContent = new GUIContent(NormalMapEditorName);
+            window.minSize = NormalMapEditorWindowSize;
+        }
+
+        private void OnEnable()
+        {
+            _root = rootVisualElement;
+
+            #region INITIALIZATION AND QUERY
+            // Root
+            var rootVisualTree = Resources.Load<VisualTreeAsset>(RootUxmlPath);
+            rootVisualTree.CloneTree(_root);
+            
+            #endregion
+        }
+
+    }
+}
